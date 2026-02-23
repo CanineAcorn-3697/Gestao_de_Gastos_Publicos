ca325e29eaa6f1853cbc002721a3a2c97ae2a135
CanineAcorn-3697 <pedro.seabra.12as@gmail.com>
2026-02-23 20:02:10 -0300

    Add: Adicionando schemas

---
+++ pipelines/transformation/schemas.py
@@ -0,0 +1,16 @@
+import pyspark.sql
+
+SCHEMA_BRONZE = StructType([
+    StructField("orgaoSuperior", StringType(), True),
+    StructField("orgao", StringType(), True),
+    StructField("valorPago", StringType(), True),
+    StructField("dataReferencia", StringType(), True),
+])
+
+SCHEMA_SILVER = StructType([
+    StructField("orgaoSuperior", StringType(), False),
+    StructField("orgao", StringType(), False),
+    StructField("valor_pago", DoubleType(), False),
+    StructField("data_referencia", DateType(), False),
+    StructField("ano_referencia", IntegerType(), False),
+])
