5ecf68fe5a4125d36c6ffd9ff3213bec63d3275e
CanineAcorn-3697 <pedro.seabra.12as@gmail.com>
2026-02-23 21:08:03 -0300

    FIX: corrigindo schemas

--- pipelines/transformation/schemas.py
+++ pipelines/transformation/schemas.py
@@ -6,14 +6,18 @@ from pyspark.sql.types import (
 SCHEMA_BRONZE = StructType([
     StructField("orgaoSuperior", StringType(), True),
     StructField("orgao", StringType(), True),
-    StructField("valorPago", StringType(), True),
-    StructField("dataReferencia", StringType(), True),
+    StructField("empenhado", StringType(), True),
+    StructField("liquidado", StringType(), True),
+    StructField("pago", StringType(), True),
+    StructField("dataReferencia", StringType(), True)
 ])
 
 SCHEMA_SILVER = StructType([
-    StructField("orgaoSuperior", StringType(), False),
+    StructField("orgao_superior", StringType(), False),
     StructField("orgao", StringType(), False),
-    StructField("valor_pago", DoubleType(), False),
+    StructField("empenhado", DoubleType(), False),
+    StructField("liquidado", DoubleType(), False),
+    StructField("pago", DoubleType(), False),
     StructField("data_referencia", DateType(), False),
-    StructField("ano_referencia", IntegerType(), False),
+    StructField("ano_referencia", IntegerType(), False)
 ])
